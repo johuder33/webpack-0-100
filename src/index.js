9a92773563af4494e8a6fd0480d9ac241756cd77
Juorder Gonzalez <johudergb@hotmail.com>
2022-01-10 00:36:46 -0300

    import react applications

--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
+import ReactDOM from 'react-dom';
+import App from './app';
 import './styles/index.scss';
-import cartoonImage from '../assets/images/cartoon.png';
 
-const config = {
-  baseUrl: 'https://evernote.com/api',
-  path: '/v1/notes'
-};
-
-class Webclipper {
-  constructor() {
-    this.map = new Map();
-    this.config = { ...config };
-  }
-
-  serialize() {
-    console.log('Webclipper is running woohooo !!', this.config);
-  }
-
-  injectImage() {
-    const img = document.getElementById('cartoon');
-    img.src = cartoonImage;
-  }
-
-  static autoinit() {
-    if (!Webclipper.instance) {
-      Webclipper.instance = new Webclipper();
-    }
-
-    return Webclipper.instance;
-  }
-}
-
-Webclipper.instance = null;
-
-export default Webclipper;
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
